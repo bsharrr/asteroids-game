5234c2226b41c45932af76dde4c4717ffa3ea74d
bsharrr <badinifranck7@gmail.com>
2024-10-09 15:05:24 -0400

    Adding FPS logic to our game

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from constants import *
 
 
 def main():
+    #Setup pygame window
     pygame.init()
     game_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Setup the game FPS
+    game = pygame.time.Clock()
+    dt = 0
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(game_window, color="black", rect=None, special_flags=0)
         pygame.display.flip()
+        game.tick(60)
+        dt = game.tick(60) / 1000
 
 
 
