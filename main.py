36fda79bae166385164ade3dc97e9dd8bca78b21
bsharrr <badinifranck7@gmail.com>
2024-10-09 15:30:52 -0400

    Adding logic for Player display

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame 
 from constants import *
-
+from player import Player
 
 def main():
     #Setup pygame window
     pygame.init()
-    game_window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
     # Setup the game FPS
     game = pygame.time.Clock()
     dt = 0
+
+    # Instantiate a player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    # Game Loop
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(game_window, color="black", rect=None, special_flags=0)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        game.tick(60)
+        
         dt = game.tick(60) / 1000
 
 
