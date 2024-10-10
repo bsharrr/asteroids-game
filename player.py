a7a4add9968d5b1033aaf04762d3b99d196fca66
bsharrr <badinifranck7@gmail.com>
2024-10-10 12:33:05 -0400

    adding the shooting logic

--- player.py
+++ player.py
@@ -1,11 +1,15 @@
 from circleshape import *
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import *
+from shot import Shot
 import pygame
+
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
@@ -25,11 +29,16 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
-
     
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+        
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
